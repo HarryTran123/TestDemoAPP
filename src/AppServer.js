78e4450407e6b7a11909f57fa6123a11f94e361b
Dungdtgcs200419 <dungdtgcs200419@fpt.edu.vn>
2022-02-26 20:33:03 +0700

    update

--- src/AppServer.js
+++ src/AppServer.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+//Declare static path
+app.use(express.static(path.join(__dirname, "public")));
+
 //Use Morgan
 app.use(morgan('combined'));
 
+//Use Template Engine - handlebars
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+//Router
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('home');
 })
 
 app.listen(port, () => {
