801c25c6546fd8b964ecdbd74cf4f6a36c68c0e3
Dungdtgcs200419 <dungdtgcs200419@fpt.edu.vn>
2022-03-01 22:05:51 +0700

    update

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const siteController = require('../app/controllers/SiteController')
+const siteController = require('../app/controllers/SiteController');
 
-router.get('/about', siteController.about)
-router.get('/products/:catalog_name', siteController.catalog)
-router.get('/product/:slug', siteController.product)
+router.get('/about', siteController.about);
+router.get('/products/:catalog_name', siteController.catalog);
+router.get('/product/:slug', siteController.product);
 
-router.get('/', siteController.home)
+router.get('/', siteController.home);
 
 module.exports = router;
