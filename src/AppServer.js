338b2fcb52880aeeddfa5c243df083c233214569
Dungdtgcs200419 <dungdtgcs200419@fpt.edu.vn>
2022-03-01 22:03:06 +0700

    update

--- src/AppServer.js
+++ src/AppServer.js
@@ -54,6 +54,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app)
 
 
-app.listen(port, () => {
-    console.log(`App created on link: http://${hostname}:${process.env.port | port}`);
-})
+app.listen(process.env.port | port);
+console.log(`App created on link: http://${hostname}:${process.env.port | port}`);
